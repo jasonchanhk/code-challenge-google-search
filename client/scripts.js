29cde779e7fe8ce3d50fe03f24a7ad3765754bb3
nouria chaouy <nouria.chaouy@gmail.com>
2022-06-29 17:04:28 +0100

    added assets and modified scripts js and styles.css

--- client/scripts.js
+++ client/scripts.js
@@ -1,16 +1,17 @@
-const form = document.querySelector('#google-search-form')
+const form = document.querySelector("#google-search-form");
 
 function searchKeyword(e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    const keyword = e.target.keyword.value
-    fetch(`http://localhost:3000/search/${keyword}`)
-        .then(r => r.json())
-        .then(appendResults)
-        .catch(console.warn)
+  const keyword = e.target.keyword.value;
+  fetch(`http://localhost:3000/search/${keyword}`)
+    .then((r) => r.json())
+    .then(appendResults)
+    .catch(console.warn);
 }
 
-{/* <div class="search-results">
+{
+  /* <div class="search-results">
     <a href="'https://en.wikipedia.org/wiki/Cat">Cat - Wikipedia</a>
     <p class="text-results">
         The cat (Felis catus) is a domestic species of small carnivorous
@@ -18,29 +19,40 @@ function searchKeyword(e) {
         is often referred to as the domestic cat to distinguish it from the
         wild members of the family.
     </p>
-</div> */}
-
-function appendResults(data){
-    document.querySelector('.search-result-container').innerHTML = ""
-    data.forEach(appendEachResult);
+</div> */
 }
 
-function appendEachResult(data){
-    const newDiv = document.createElement('div')
-    newDiv.setAttribute('class', 'search-results')
-    
-    const newATag = document.createElement('a')
-    newATag.setAttribute('href', data.link)
-    newATag.textContent = data.title
-
-    const newPara = document.createElement('p')
-    newPara.setAttribute('class', 'text-results')
-    newPara.textContent = data.description
-
-    newDiv.append(newATag, newPara)
-
-    const resultContainer = document.querySelector('.search-result-container')
-    resultContainer.append(newDiv)
+function setAttributes(el, attrs) {
+  for (let key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
 }
 
-form.addEventListener('submit', searchKeyword)
+function appendResults(data) {
+  document.querySelector(".search-result-container").innerHTML = "";
+  data.forEach(appendEachResult);
+}
+
+function appendEachResult(data) {
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "search-results");
+
+  const aTag = document.createElement("a");
+  setAttributes(aTag, { href: data.link, class: "first-tag" });
+  aTag.textContent = data.link;
+
+  const newATag = document.createElement("a");
+  newATag.setAttribute("href", data.link);
+  newATag.textContent = data.title;
+
+  const newPara = document.createElement("p");
+  newPara.setAttribute("class", "text-results");
+  newPara.textContent = data.description;
+
+  newDiv.append(aTag, newATag, newPara);
+
+  const resultContainer = document.querySelector(".search-result-container");
+  resultContainer.append(newDiv);
+}
+
+form.addEventListener("submit", searchKeyword);
